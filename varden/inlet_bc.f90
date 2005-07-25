aeb07c3e0c914975c1f81fab95923da6e6b5a0b2
almgren <almgren>
2005-07-25 16:52:04 +0000

    Zero on every side...

--- varden/inlet_bc.f90
+++ varden/inlet_bc.f90
@@ -4,7 +4,7 @@ module inlet_bc
    implicit none
 
    real(kind=dp_t), parameter :: INLET_VX  =  0.0d0
-   real(kind=dp_t), parameter :: INLET_VY  =  1.0d0
+   real(kind=dp_t), parameter :: INLET_VY  =  0.0d0
    real(kind=dp_t), parameter :: INLET_DEN =  1.0d0
    real(kind=dp_t), parameter :: INLET_TRA =  1.0d0
 
