b7c3c4571547784396be905373356bafe7ea1979
lijewski <lijewski>
2007-12-10 20:37:16 +0000

    added bl_prof stuff

--- varden/test_multilevel/main.f90
+++ varden/test_multilevel/main.f90
@@ -1,11 +1,17 @@
 program main
+
   use BoxLib
+  use bl_prof_module
+
   implicit none
 
   call boxlib_initialize()
+  call bl_prof_initialize(on = .true.)
 
   call varden()
 
+  call bl_prof_glean("bl_prof_res")
+  call bl_prof_finalize()
   call boxlib_finalize()
 
 end program main
