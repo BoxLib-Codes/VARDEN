7af57ba51d20cd7419f7e5571384dcef8d911bdd
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-02-09 16:35:14 -0800

    optimize nested loops

--- src/multifab_physbc.f90
+++ src/multifab_physbc.f90
@@ -167,14 +167,14 @@ contains
                (15.0_dp_t*s(i,lo(2)) - 10.0_dp_t*s(i,lo(2)+1) + 3.0_dp_t*s(i,lo(2)+2))
        end do
     else if (bc(2,1) .eq. REFLECT_EVEN) then
-       do i = lo(1)-ng, hi(1)+ng
-          do j = 1, ng
+       do j = 1, ng
+          do i = lo(1)-ng, hi(1)+ng
              s(i,lo(2)-j) = s(i,lo(2)+j-1)
           end do
        end do
     else if (bc(2,1) .eq. REFLECT_ODD) then
-       do i = lo(1)-ng, hi(1)+ng
-          do j = 1, ng
+       do j = 1, ng
+          do i = lo(1)-ng, hi(1)+ng
              s(i,lo(2)-j) = -s(i,lo(2)+j-1)
           end do
        end do
@@ -200,14 +200,14 @@ contains
                (15.0_dp_t*s(i,hi(2)) - 10.0_dp_t*s(i,hi(2)-1) + 3.0_dp_t*s(i,hi(2)-2))
        end do
     else if (bc(2,2) .eq. REFLECT_EVEN) then
-       do i = lo(1)-ng, hi(1)+ng
-          do j = 1, ng
+       do j = 1, ng
+          do i = lo(1)-ng, hi(1)+ng
              s(i,hi(2)+j) = s(i,hi(2)-j+1)
           end do
        end do
     else if (bc(2,2) .eq. REFLECT_ODD) then
-       do i = lo(1)-ng, hi(1)+ng
-          do j = 1, ng
+       do j = 1, ng
+          do i = lo(1)-ng, hi(1)+ng
              s(i,hi(2)+j) = -s(i,hi(2)-j+1)
           end do
        end do
@@ -373,16 +373,16 @@ contains
        end do
     else if (bc(2,1) .eq. REFLECT_EVEN) then
        do k = lo(3)-ngzlo,hi(3)+ngzhi
-          do i = lo(1)-ng,hi(1)+ng
-             do j = 1,ng
+          do j = 1,ng
+             do i = lo(1)-ng,hi(1)+ng
                 s(i,lo(2)-j,k) = s(i,lo(2)+j-1,k)
              end do
           end do
        end do
     else if (bc(2,1) .eq. REFLECT_ODD) then
        do k = lo(3)-ngzlo,hi(3)+ngzhi
-          do i = lo(1)-ng,hi(1)+ng
-             do j = 1,ng
+          do j = 1,ng
+             do i = lo(1)-ng,hi(1)+ng
                 s(i,lo(2)-j,k) = -s(i,lo(2)+j-1,k)
              end do
           end do
@@ -417,16 +417,16 @@ contains
        end do
     else if (bc(2,2) .eq. REFLECT_EVEN) then
        do k = lo(3)-ngzlo,hi(3)+ngzhi
-          do i = lo(1)-ng,hi(1)+ng
-             do j = 1,ng
+          do j = 1,ng
+             do i = lo(1)-ng,hi(1)+ng
                 s(i,hi(2)+j,k) = s(i,hi(2)-j+1,k)
              end do
           end do
        end do
     else if (bc(2,2) .eq. REFLECT_ODD) then
        do k = lo(3)-ngzlo,hi(3)+ngzhi
-          do i = lo(1)-ng,hi(1)+ng
-             do j = 1,ng
+          do j = 1,ng
+             do i = lo(1)-ng,hi(1)+ng
                 s(i,hi(2)+j,k) = -s(i,hi(2)-j+1,k)
              end do
           end do
@@ -460,17 +460,17 @@ contains
           end do
        end do
     else if (bc(3,1) .eq. REFLECT_EVEN) then
-       do j = lo(2)-ng,hi(2)+ng
-          do i = lo(1)-ng,hi(1)+ng
-             do k = 1,ng
+       do k = 1,ng
+          do j = lo(2)-ng,hi(2)+ng
+             do i = lo(1)-ng,hi(1)+ng
                 s(i,j,lo(3)-k) = s(i,j,lo(3)+k-1)
              end do
           end do
        end do
     else if (bc(3,1) .eq. REFLECT_ODD) then
-       do j = lo(2)-ng,hi(2)+ng
-          do i = lo(1)-ng,hi(1)+ng
-             do k = 1,ng
+       do k = 1,ng
+          do j = lo(2)-ng,hi(2)+ng
+             do i = lo(1)-ng,hi(1)+ng
                 s(i,j,lo(3)-k) = -s(i,j,lo(3)+k-1)
              end do
           end do
@@ -504,17 +504,17 @@ contains
           end do
        end do
     else if (bc(3,2) .eq. REFLECT_EVEN) then
-       do j = lo(2)-ng,hi(2)+ng
-          do i = lo(1)-ng,hi(1)+ng
-             do k = 1,ng
+       do k = 1,ng
+          do j = lo(2)-ng,hi(2)+ng
+             do i = lo(1)-ng,hi(1)+ng
                 s(i,j,hi(3)+k) = s(i,j,hi(3)-k+1)
              end do
           end do
        end do
     else if (bc(3,2) .eq. REFLECT_ODD) then
-       do j = lo(2)-ng,hi(2)+ng
-          do i = lo(1)-ng,hi(1)+ng
-             do k = 1,ng
+       do k = 1,ng
+          do j = lo(2)-ng,hi(2)+ng
+             do i = lo(1)-ng,hi(1)+ng
                 s(i,j,hi(3)+k) = -s(i,j,hi(3)-k+1)
              end do
           end do
