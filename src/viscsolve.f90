ac315c4b5689e3fd219749a98f034e5de21aac40
Ann Almgren <asalmgren@lbl.gov>
2016-04-05 12:30:15 -0700

    fine_flx now starts at level 1, not level 2.

--- src/viscsolve.f90
+++ src/viscsolve.f90
@@ -322,7 +322,7 @@ contains
     ! Local  
     type(multifab)  :: rh(mla%nlevel),phi(mla%nlevel)
     type(multifab)  :: alpha(mla%nlevel),beta(mla%nlevel,mla%dim)
-    type(bndry_reg) :: fine_flx(2:mla%nlevel)
+    type(bndry_reg) :: fine_flx(mla%nlevel)
     integer         :: n,d,nlevs,dm,ng_cell
     real(kind=dp_t) :: rel_solver_eps
     real(kind=dp_t) :: abs_solver_eps
@@ -364,7 +364,7 @@ contains
        call mkrhs(rh(n),snew(n),laps(n),phi(n),mu,icomp)
     end do
 
-    do n = 2,nlevs
+    do n = 1,nlevs
        call bndry_reg_build(fine_flx(n),mla%la(n),ml_layout_get_pd(mla,n))
     end do
 
@@ -416,7 +416,7 @@ contains
        end do
     end do
 
-    do n = 2,nlevs
+    do n = 1,nlevs
        call bndry_reg_destroy(fine_flx(n))
     end do
 
